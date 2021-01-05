b75c49737ce4f0df5695e27f3a61a2ebcace98cf
bazilucmarian <bazilucmarian98@yahoo.com>
2021-01-05 20:30:53 +0200

    new

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ app.use(bodyParser.json());
 const port = process.env.PORT || 5000;
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/fish-inventory/build")));
+  app.use(express.static("fish-inventory/build");
   app.get("*", (req, res) =>
     res.sendFile(
       path.resolve(__dirname, "fish-inventory", "build", "index.html")
