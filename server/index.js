27871431e0273c6ea23a732847bf96f8636bc818
bazilucmarian <bazilucmarian98@yahoo.com>
2021-01-05 21:01:23 +0200

    try again resolve with get request

--- server/index.js
+++ server/index.js
@@ -21,9 +21,7 @@ const port = process.env.PORT || 5000;
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("fish-inventory/build"));
   app.get("*", (req, res) =>
-    res.sendFile(
-      path.resolve(__dirname, "fish-inventory", "build", "index.html")
-    )
+    res.sendFile(path.resolve("fish-inventory", "build", "index.html"))
   );
 }
 
